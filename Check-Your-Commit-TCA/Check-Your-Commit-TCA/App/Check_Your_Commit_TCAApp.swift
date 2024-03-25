d21552afad291d21f049c28402fe2e42c8d1b573
Hsungjin <hsungjin142@gmail.com>
2024-03-25 16:40:47 +0900

    [Add] 파일구조 추가

---
+++ Check-Your-Commit-TCA/Check-Your-Commit-TCA/App/Check_Your_Commit_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check_Your_Commit_TCAApp.swift
+//  Check-Your-Commit-TCA
+//
+//  Created by 황성진 on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Check_Your_Commit_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
